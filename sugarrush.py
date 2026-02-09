28adb764bae22a86965d844c262b53dc72460ea4
pat <patrickhingley@live.co.uk>
2026-02-09 02:52:58 +0000

    tightened typecheck

--- sugarrush.py
+++ sugarrush.py
@@ -22,13 +22,16 @@ class model_step_result:
 		return '{} epoch {}/{}{} '.format(self.type, self.epoch, self.config.epoch_count, step_str)
 
 def register_train_step_result(result:model_step_result, config: model_config, result_list:dict[str,list[model_step_result]], log_interval:dict[str, int]):
+	# Add result to relevant list
 	result.config = config
 	if not result.type in result_list:
 		result_list[result.type] = []
 	result_list[result.type].append(result)
-	if (result.step == None) or \
-		(not result.type in log_interval) or \
-		(result.step % log_interval[result.type] == 0):
+	
+	# Log if if there is no registered step for this result, we don't have a log interval for this type, or the log interval lines up
+	if result.step is None or \
+		not result.type in log_interval or \
+		result.step % log_interval[result.type] == 0:
 		print(result)
 
 # TODO: type gore
