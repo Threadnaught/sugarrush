33cf001762a2863b4990166f0db3864b99db7453
pat <patrickhingley@live.co.uk>
2026-02-10 23:14:49 +0000

    changed to dict based. p cool now.

--- sugarrush.py
+++ sugarrush.py
@@ -1,50 +1,50 @@
 from typing import *
+import numpy as np
+import torch # todo should this be try import?
 import json
 
-# A SUPER simple library for doing hyperparameter searches
+model_config_t = dict[str, Any]
+result_t = dict[str, Union[int, float, torch.Tensor, np.ndarray]] # todo report np arrays
+report_result_t = Callable[[str, result_t], None]
+train_individual_config_t = Callable[[model_config_t, report_result_t], Any]
 
-class model_config:
-	def __init__(self, epoch_count:int):
-		self.epoch_count = epoch_count
-		pass
-
-class step_result:
-	def __init__(self, type:str, epoch:int, step:int | None):
-		self.type = type
-		self.epoch = epoch
-		self.step = step
-		self.config: model_config | None = None
+def report_result(result_type:str, result:result_t, config_results:dict[str, list[result_t]], log_intervals:dict[str, int]):
+	# Append a new list if needed:
+	if not result_type in config_results:
+		config_results[result_type] = []
 	
-	def __str__(self):
-		step_str = ''
-		if self.step != None:
-			step_str = ' step {}'.format(self.step)
-		return '{} epoch {}/{}{} '.format(self.type, self.epoch, self.config.epoch_count, step_str)
+	# Convert everything to a nice easy numpy array:
+	result_narrowed: dict[str, np.array] = {}
+	for key in result:
+		value = result[key]
+		if isinstance(value, int) or isinstance(value, float):
+			result_narrowed[key] = np.array(value)
+		elif isinstance(value, torch.Tensor):
+			result_narrowed[key] = value.detach().cpu().numpy()
+		elif isinstance(value, np.ndarray):
+			result_narrowed[key] = value
+		else:
+			raise Exception('Result\'s key {} of value {} has unsupported type {}'.format(key, value, type(value)))
+	config_results[result_type].append(result_narrowed)
 
-def register_train_step_result(result:step_result, config: model_config, result_list:dict[str,list[step_result]], log_interval:dict[str, int]):
-	# Add result to relevant list
-	result.config = config
-	if not result.type in result_list:
-		result_list[result.type] = []
-	result_list[result.type].append(result)
-	
-	# Log if if there is no registered step for this result, we don't have a log interval for this type, or the log interval lines up
-	if result.step is None or \
-		not result.type in log_interval or \
-		result.step % log_interval[result.type] == 0:
-		print(result)
+	# Calculate index for reporting:
+	report_i = len(config_results)
+	if 'batch' in result_narrowed:
+		report_i = result_narrowed['batch']
+	elif 'epoch' in result_narrowed:
+		report_i = result_narrowed['epoch']
 
-# TODO: type gore
-def run_training(
-	train_individual_config:Callable[[model_config, Callable[[step_result], None]], None],
-	configs: list[model_config],
-	log_interval: dict[str, int]
-) -> list[dict[str,list[step_result]]]:
-	results: list[dict[str,list[step_result]]] = []
+	# Log if needed:
+	if (not result_type in log_intervals) or (report_i % log_intervals[result_type] == 0):
+		# print(result_type, result_i, result_narrowed)
+		print(result_type, ', '.join(['{}:{}'.format(key, result_narrowed[key]) for key in result_narrowed]))
+
+def new_run_training(
+	train_individual_config:train_individual_config_t,
+	configs:list[model_config_t],
+	log_intervals: dict[str, int]
+):
 	for config in configs:
-		# Once again the map/object distinction in python bites me
-		print('Running config ', json.dumps(config.__dict__))
-		results.append({})
-		train_individual_config(config, lambda result:register_train_step_result(result, config, results[-1], log_interval))
-	
-	return results
+		print('using config ', json.dumps(config))
+		config_results:dict[str, list[result_t]] = {}
+		train_individual_config(config, lambda report_type, result: report_result(report_type, result, config_results, log_intervals))
