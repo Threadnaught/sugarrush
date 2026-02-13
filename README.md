d690cdd332db5c3ba0970468b50b8c4e580cacfd
pat <patrickhingley@live.co.uk>
2026-02-13 01:28:27 +0000

    comments and readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Sugarrush
 
-Dead simple hyperparameter iterator and logger. You give it configs and reports, it prints out how your training is going.
+Dead simple hyperparameter iterator and logger. You give it configs and reports, it logs how your training is going and allows you to compare between runs at the end. See `sugarrush-example.py` to see how easy it is to use.
--- sugarrush-example.py
+++ sugarrush-example.py
@@ -3,23 +3,27 @@ import random
 import matplotlib.pyplot as plt
 from typing import *
 
+# Define your training function:
 def train_individual_config(config, report) -> None:
     for epoch in range(10):
         for batch in range(100):
             report(
                 'train', # what category of info this is
-                {'batch':batch, 'epoch':epoch, 'loss':random.uniform(0,config['max'])} # info to report
+                {'batch':batch, 'epoch':epoch, 'loss':random.uniform(0,config['max'])} 
             )
         report('test', {'epoch':epoch, 'loss':random.uniform(0,config['max'])})
 
+# Run the training
 results, _ = sr.run_training(
     train_individual_config, # Your training function
     [{'max':2},{'max':5}], # Your configs to train
     {'train':10} # Your log interval (missing implies reporting logging every time)
 )
 
+# Convert the config reports into numpy arrays
 zeroth_losses, first_losses = sr.extract_column_all_configs(results, 'train', 'loss')
 
+# Plot them
 plt.plot(first_losses)
 plt.plot(zeroth_losses)
 
