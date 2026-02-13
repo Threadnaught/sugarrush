151a05a7bd5134f1dd66ea5b1dc610946db1195a
pat <patrickhingley@live.co.uk>
2026-02-13 01:26:07 +0000

    added example

---
+++ sugarrush-example.py
@@ -0,0 +1,26 @@
+import sugarrush as sr
+import random
+import matplotlib.pyplot as plt
+from typing import *
+
+def train_individual_config(config, report) -> None:
+    for epoch in range(10):
+        for batch in range(100):
+            report(
+                'train', # what category of info this is
+                {'batch':batch, 'epoch':epoch, 'loss':random.uniform(0,config['max'])} # info to report
+            )
+        report('test', {'epoch':epoch, 'loss':random.uniform(0,config['max'])})
+
+results, _ = sr.run_training(
+    train_individual_config, # Your training function
+    [{'max':2},{'max':5}], # Your configs to train
+    {'train':10} # Your log interval (missing implies reporting logging every time)
+)
+
+zeroth_losses, first_losses = sr.extract_column_all_configs(results, 'train', 'loss')
+
+plt.plot(first_losses)
+plt.plot(zeroth_losses)
+
+plt.show()
